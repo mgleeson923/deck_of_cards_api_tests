0572a54c6c88dbfcd60ab0b233024cbc1a7bcc97
mgleeson923 <mgleeson923@gmail.com>
2023-09-28 22:05:04 -0400

    Added steps for editing card values (changed face card names to value of 10), and converted hand into integers to add for a total.

--- deckofcards/tests/test_deal_cards_to_players.py
+++ deckofcards/tests/test_deal_cards_to_players.py
@@ -1,3 +1,5 @@
+from operator import index
+
 import pytest
 import requests
 import pdb
@@ -16,36 +18,67 @@ def test_draw_cards_from_deck():
     assert shuffle_response_body['shuffled'] is True
     assert shuffle_response_body['remaining'] is 52
 
+    ## Create player one's hand
     draw_response = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id + '/draw/?count=3')
     draw_response_body = draw_response.json()
     cards = draw_response_body['cards']
-    logger.info(cards)
     pile_one = 'pile_one'
 
+    player_one_cards = []
     for card in cards:
         assert len(card['code']) is 2
         cards_response_one = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id +
                                           '/pile/' + pile_one + '/add/?cards=' + card['code'])
         assert cards_response_one.status_code is 200
+        player_one_cards.append(card['value'])
+    logger.debug(player_one_cards)
 
-    player_one_hand = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id +
-                                   '/pile/' + pile_one + '/list/')
-    response_body = player_one_hand.json()
-    logger.info(response_body)
+    i = 0
+    while i < len(player_one_cards):
+        if player_one_cards[i] == 'KING':
+            player_one_cards[i] = '10'
+        if player_one_cards[i] == 'QUEEN':
+            player_one_cards[i] = '10'
+        if player_one_cards[i] == 'JACK':
+            player_one_cards[i] = '10'
+        if player_one_cards[i] == 'ACE':
+            player_one_cards[i] = '10'
+        i += 1
 
+    player_one_cards = [eval(i) for i in player_one_cards]
+    logger.debug(player_one_cards)
+    player_one_hand_value = sum(player_one_cards)
+    logger.debug(player_one_hand_value)
+
+    ## Create player two's hand
     draw_response = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id + '/draw/?count=3')
     draw_response_body = draw_response.json()
     cards = draw_response_body['cards']
     logger.info(cards)
     pile_two = 'pile_two'
 
+    player_two_cards = []
     for card in cards:
         assert len(card['code']) is 2
         cards_response_two = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id +
                                           '/pile/' + pile_two + '/add/?cards=' + card['code'])
         assert cards_response_two.status_code is 200
-    player_two_hand = requests.get('https://deckofcardsapi.com/api/deck/' + deck_id +
-                                   '/pile/' + pile_two + '/list/')
-    response_body = player_two_hand.json()
+        player_two_cards.append(card['value'])
+    logger.debug(player_two_cards)
 
-    logger.info(response_body)
+    i = 0
+    while i < len(player_two_cards):
+        if player_two_cards[i] == 'KING':
+            player_two_cards[i] = '10'
+        if player_two_cards[i] == 'QUEEN':
+            player_two_cards[i] = '10'
+        if player_two_cards[i] == 'JACK':
+            player_two_cards[i] = '10'
+        if player_two_cards[i] == 'ACE':
+            player_two_cards[i] = '10'
+        i += 1
+
+    player_two_cards = [eval(i) for i in player_two_cards]
+    logger.debug(player_two_cards)
+    player_two_hand_value = sum(player_two_cards)
+    logger.debug(player_two_hand_value)
